9c3552afa76637a71f120302667913e2def454a1
ATHIRA K S <athirakolathery2002@gmail.com>
2026-04-01 13:03:36 +0530

    Removed Contract-ui folder

--- Contract-ui/contract-manager/Contract-api/contract/contract/src/main/java/com/example/contract/repository/ContractServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.contract.repository;
-
-import com.example.contract.entity.ContractService;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractServiceRepository extends JpaRepository<ContractService, Long> {
-}
